c395ba51245973621e728cd984b1965f666df300
cjgarcia12 <christian.jgarcia03@gmail.com>
2025-02-10 00:35:38 -0500

    Init commit for my invoice tracker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {
+  ClerkProvider,
+} from "@clerk/nextjs";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,16 +11,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className='bg-[#09090b]'>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Image from "next/image";
+
+type Props = {
+  children: React.ReactNode;
+};
+const Header: React.FC<Props> = ({ children }) => {
+  return (
+    <header className="flex items-center justify-center w-full h-[50px]">
+        <div className='flex w-5/6 justify-between'>
+            {children}
+            <Image
+                src="/logo.png"
+                alt="Logo with name"
+                width={150}
+                height={150}
+                className="hidden md:block"
+            />
+            <Image
+                src="/logosmall.png"
+                alt="Logo"
+                width={100}
+                height={100}
+                className="mr-2 md:hidden"
+            />
+        </div>
+    </header>
+  );
+};
+export default Header;
